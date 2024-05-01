cfa0fcc03a308e12e6c6300aac7795bc2489943b
Sanket Singh <singhsanket143@gmail.com>
2024-05-01 22:48:17 +0530

    Room creation logic with user joining the room and there record of joining being stored added

--- src/Context/SocketContext.tsx
+++ src/Context/SocketContext.tsx
@@ -1,7 +1,8 @@
 import SocketIoClient from "socket.io-client";
-import { createContext, useEffect } from "react";
+import { createContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import Peer from "peerjs";
+import { v4 as UUIDv4 } from "uuid";
 const WS_Server = "http://localhost:5500";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,7 +20,21 @@ interface Props {
 export const SocketProvider: React.FC<Props> = ({ children }) => {
 
     const navigate = useNavigate(); // will help to programatically handle navigation
+    
+    // state variable to store the userId 
+    const [user, setUser] = useState<Peer>(); // new peer user
+
+    const fetchParticipantList = ({roomId, participants}: {roomId: string, participants: string[]}) => {
+        console.log("Fetched room participants");
+        console.log(roomId, participants);
+    }
+
     useEffect(() => {
+
+        const userId = UUIDv4();
+        const newPeer = new Peer(userId);
+
+        setUser(newPeer);
 
         const enterRoom = ({ roomId} : { roomId: string}) => {
             navigate(`/room/${roomId}`); 
@@ -27,10 +42,12 @@ export const SocketProvider: React.FC<Props> = ({ children }) => {
 
         // we will transfer the user to the room page when we collect an event of room-created from server
         socket.on("room-created", enterRoom);
+
+        socket.on("get-users", fetchParticipantList);
     }, []);
 
     return (
-        <SocketContext.Provider value={{ socket }}>
+        <SocketContext.Provider value={{ socket, user }}>
             {children}
         </SocketContext.Provider>
     );
--- src/Pages/Room.tsx
+++ src/Pages/Room.tsx
@@ -5,14 +5,19 @@ import { SocketContext } from "../Context/SocketContext";
 const Room: React.FC = () => {
 
     const { id } = useParams();
-    const { socket } = useContext(SocketContext);
+    const { socket, user } = useContext(SocketContext);
 
     useEffect(() => {
         // emitting this event so that either creator of room or joinee in the room 
         // anyone is added the server knows that new people have been added\
         // to this room
-        socket.emit("joined-room", {roomId: id});
-    }, []); 
+        if(user) {
+            console.log("New user with id", user._id, "has joined room", id);
+            socket.emit("joined-room", {roomId: id, peerId: user._id})
+        }
+
+
+    }, [id, user, socket]); 
 
     return(
         <div>
